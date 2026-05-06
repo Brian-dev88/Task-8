3a68d9748d925fb9077495e7df41b063bb523171
Brian-dev88 <24255@burnside.school.nz>
2026-05-07 09:20:11 +1200

    bob

--- test-1.py
+++ test-1.py
@@ -1,6 +1,18 @@
+#imports
 import _sqlite3
 
-db = _sqlite3.connect("student.db")
+#variables
+DATABASE = "student.db"
+
+
+
+#function
+
+
+
+
+#main code
+db = _sqlite3.connect(DATABASE)
 cursor = db.cursor()
 sql = "SELECT * FROM student;"
 cursor.execute(sql)
