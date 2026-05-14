16f1e4951ca12da981cb88ac62c85f16a356e7cb
Brian-dev88 <24255@burnside.school.nz>
2026-05-14 12:29:51 +1200

    Sutdent_id and name

--- test-1.py
+++ test-1.py
@@ -1,18 +1,21 @@
 import sqlite3
 
-db = sqlite3.connect("student.db")
+db = sqlite3.connect("student (1).db")
 cursor = db.cursor()
 
-student_name = input("Enter the name of the student: ")
+grade = input("Enter the name of the student: ")
 
-sql = "SELECT * FROM grade WHERE student_name = ?"
-cursor.execute(sql, (student_name,))
+sql = "SELECT * FROM student WHERE student_name = ?"
+cursor.execute(sql, (grade,))
 
 results = cursor.fetchall()
 
 if results:
-    print(results)
+    print(f"{'student_id':<15}{'student_name':<25}")
+    print("-" * 40)
+
+    for row in results:
+        print(f"{row[0]:<15}{row[1]:<25}")
 else:
     print("No records found.")
 
-db.close()
