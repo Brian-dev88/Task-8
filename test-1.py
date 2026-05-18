f6acdad234db080e80d5209f557fe8e5273f4412
Brian-dev88 <24255@burnside.school.nz>
2026-05-18 12:31:36 +1200

    Grade_score

--- test-1.py
+++ test-1.py
@@ -5,17 +5,16 @@ cursor = db.cursor()
 
 grade = input("Enter the name of the student: ")
 
-sql = "SELECT * FROM student WHERE student_name = ?"
+sql = "SELECT * FROM student, grade WHERE student_name = ? AND student.student_id = grade.student_id"
 cursor.execute(sql, (grade,))
 
 results = cursor.fetchall()
 
 if results:
-    print(f"{'student_id':<15}{'student_name':<25}")
-    print("-" * 40)
+    print(f"{'student_id':<15}{'student_name':<25}{'grade_score':<25}")
+    print("-" * 50)
 
     for row in results:
-        print(f"{row[0]:<15}{row[1]:<25}")
+        print(f"{row[0]:<15}{row[1]:<25}{row[5]:<25}")
 else:
     print("No records found.")
-
